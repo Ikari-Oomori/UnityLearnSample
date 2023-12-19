cf4548c1d87ce06b5b17d303012c9eaeb433a2a8
Diana <sergeeva.diana228@gmail.com>
2023-12-19 10:49:48 +0700

    Add 5 lab (SampleScene, Scripts)

--- Assets/LearnMaterials 2/Scripts/DestroyModule.cs
+++ Assets/LearnMaterials 2/Scripts/DestroyModule.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [HelpURL("https://docs.google.com/document/d/1RMamVxE-yUpSfsPD_dEa4-Ak1qu6NTo83qY1O4XLxUY/edit?usp=sharing")]
 public class DestroyModule : MonoBehaviour
 {
-    private float destroyDelay;
-    private int minimalDestroyingObjectsCount;
+    [SerializeField, Range(0.1f, 10f)]
+    private float destroyDelay = 1.0f;
+
+    [SerializeField, Range(1, 100)]
+    private int minimalDestroyingObjectsCount = 10;
 
     private Transform myTransform;
 
@@ -28,5 +33,13 @@ public class DestroyModule : MonoBehaviour
             yield return new WaitForSeconds(destroyDelay);
         }
         Destroy(gameObject, Time.deltaTime);
+    }
+
+    // Контекстное меню для запуска удаления объектов
+    [ContextMenu("Начать удаление объектов")]
+    private void ContextMenuStartDestroyObjects()
+    {
+        Console.WriteLine("Начинаю удалять объекты");
+        ActivateModule();
     }
 }
---
+++ Assets/LearnMaterials 2/Scripts/Diana_Scirpts/CopyScript2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyScript2 : SampleScript2
+{
+    public GameObject prefab; // Префаб для создания
+    public float spacing; // Расстояние между объектами по оси Z
+    public int amount; // Количество создаваемых объектов
+
+
+
+
+    [ContextMenu("Старт")]
+    public override void Use()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            // Создание объекта
+            GameObject go = Instantiate(prefab);
+
+            go.transform.position = transform.position + new Vector3(0, 0, spacing * i);
+        }
+    }
+
+
+}
